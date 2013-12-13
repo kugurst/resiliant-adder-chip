4aba2dc4f9370fb73501cffbc07553cc94487472
kugurst <ma2799@gmail.com>
2013-12-13 10:59:30 -0500

    Just made the first argument the active variable

--- simulate.sh
+++ simulate.sh
@@ -3,10 +3,11 @@
 spareArr=( 1 2 4 8 )
 kArr=( 16 64 256 )
 
-rm results-$1.txt
+active=$1
+rm results-$active.txt
 
 for spare in "${spareArr[@]}"; do
     for k in "${kArr[@]}"; do
-        java MTTFAdder "$((spare * active))" "$1" "$k" >> results-$1.txt
+        java MTTFAdder "$((spare * active))" "$active" "$k" >> results-$active.txt
     done
 done
